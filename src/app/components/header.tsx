9d183cfc09139cf9929a051ffcd4d810eb04c662
mukesh <mukeshnarravula14@gmail.com>
2025-04-27 16:06:41 +0530

    Made the header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+    return (
+      <header className="bg-gray-800 shadow-md h-16 flex items-center justify-between px-6">
+        <div className="text-xl font-bold text-white">
+          Code-Snippet-Manager
+        </div>
+      </header>
+    );
+  };
+  
+  export default Header;
+  
