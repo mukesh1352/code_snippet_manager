27c423e32d32429eb93d48c6ea86e2afea93c758
mukesh <mukeshnarravula14@gmail.com>
2025-05-04 20:17:19 +0530

    Added the signup button with the redirection

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,13 +1,25 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Button } from '../components/ui/button';
 
 const Header = () => {
-    return (
-      <header className="bg-gray-800 shadow-md h-16 flex items-center justify-between px-6">
-        <div className="text-xl font-bold text-white">
-          Code-Snippet-Manager
-        </div>
-      </header>
-    );
-  };
-  
-  export default Header;
-  
+  const router = useRouter();
+
+  return (
+    <header className="bg-gray-800 shadow-md h-16 flex items-center justify-between px-6">
+      <div className="text-xl font-bold text-white">
+        Code-Snippet-Manager
+      </div>
+      <Button
+        variant="default"
+        onClick={() => router.push('/signup')}
+        className="bg-white text-gray-800 hover:bg-gray-200"
+      >
+        Sign Up
+      </Button>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Header from "./components/header";
-import CodeSaver from "./components/code_saver"; // Rename to PascalCase
-import Signup from "./signup/page";
+import CodeSaver from "./components/code_saver";
 export default function Home() {
   return (
     <>
       <Header />
       <CodeSaver />
-      <Signup/>
     </>
   );
 }
