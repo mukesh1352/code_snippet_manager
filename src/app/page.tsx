e3c9582e9307cb303052aa2aa1fa7036f7da09ba
mukesh <mukeshnarravula14@gmail.com>
2025-05-04 20:08:43 +0530

    Started the signup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "./components/header";
 import CodeSaver from "./components/code_saver"; // Rename to PascalCase
-
+import Signup from "./signup/page";
 export default function Home() {
   return (
     <>
       <Header />
       <CodeSaver />
+      <Signup/>
     </>
   );
 }
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+export default function Signup() {
+    return (
+        <div>
+            {
+                <>
+                    <p>Username</p>
+                    <input type="textbox"></input>
+                    <p>Password</p>
+                    <input type="textbox"></input>
+                </>
+            }
+        </div>
+    );
+}
