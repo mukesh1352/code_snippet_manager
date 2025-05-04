c9e247b3887fc424b76d5d81f9d2a9e142090293
mukesh <mukeshnarravula14@gmail.com>
2025-05-04 09:37:42 +0530

    Added the new module

--- src/app/components/code_saver.tsx
+++ src/app/components/code_saver.tsx
@@ -1,7 +1,8 @@
-const code_saver = () => {
+const Code_saver = () => {
     return (
-        <div className="text-10">
+        <div className="text-10 border-dotted">
+            <p>Hello world1</p>
         </div>
     );
 };
-export default code_saver;
+export default Code_saver;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Header from "./components/header";
+import CodeSaver from "./components/code_saver"; // Rename to PascalCase
 
 export default function Home() {
   return (
     <>
       <Header />
-      <p>Hello world</p>
+      <CodeSaver />
     </>
   );
 }
